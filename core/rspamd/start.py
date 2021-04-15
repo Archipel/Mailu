dffe2d5f27966fcd2f91794dd1ff969da6667b03
Thomas Weyn <thomas@weynwebworks.com>
2021-04-15 21:05:27 +0200

    Use overrides.d/blacklist_* files instead of local.d/blacklist_* files so the user can adapt them easily through the adminpanel

--- core/rspamd/start.py
+++ core/rspamd/start.py
@@ -18,16 +18,5 @@ if os.environ.get("ANTIVIRUS") == 'clamav':
 for rspamd_file in glob.glob("/conf/*"):
     conf.jinja(rspamd_file, os.environ, os.path.join("/etc/rspamd/local.d", os.path.basename(rspamd_file)))
 
-# blacklists
-blacklist_domains = os.environ.get("RSPAMD_BLACKLIST_DOMAINS", "").split(",")
-with open('/etc/rspamd/blacklist_domains.inc', 'w') as f:
-    for item in blacklist_domains:
-        f.write("%s\n" % item.strip())
-
-blacklist_emails = os.environ.get("RSPAMD_BLACKLIST_EMAILS", "").split(",")
-with open('/etc/rspamd/blacklist_emails.inc', 'w') as f:
-    for item in blacklist_emails:
-        f.write("%s\n" % item.strip())
-
 # Run rspamd
 os.execv("/usr/sbin/rspamd", ["rspamd", "-i", "-f"])
